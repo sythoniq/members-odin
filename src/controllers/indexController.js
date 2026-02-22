80bf4764beda49058f2009636f005441b15bd2a5
sythoniq <188770367+sythoniq@users.noreply.github.com>
2026-02-22 18:03:08 +0300

    restarting to fix structure

--- src/controllers/indexController.js
+++
@@ -1,9 +0,0 @@
-function renderIndex(req, res, next) {
-  res.render("index", {
-    title: "Welcome"
-  })
-}
-
-module.exports = {
-  renderIndex
-}
--- src/routes/index.js
+++
@@ -1,7 +0,0 @@
-const { Router } = require('express');
-const index = Router();
-const controller = require("../controllers/indexController")
-
-index.get("/", controller.renderIndex);
-
-module.exports = index;
